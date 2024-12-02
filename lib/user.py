e9fd49919613a8159ccef7036edeb301030fc6b1
amandalocher <akent22000@gmail.com>
2024-12-02 13:02:46 -0500

    completed lab

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
+
+# Define the User class such that a user is instantiated with a first_name and last_name. These should be saved as attributes.
+
+
 class User:
-    pass
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
